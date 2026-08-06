bebaab078ec1f78c4ff93c7cb21ad4645c76d0d7
Tomás Gigli <tomasgigli@yahoo.com.ar>
2026-08-06 19:40:32 -0300

    Crear .env para BD

--- app/db.py
+++ app/db.py
@@ -2,6 +2,9 @@ import os
 import mysql.connector
 from mysql.connector import Error
 import logging
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Configurar logging básico
 logging.basicConfig(level=logging.INFO)
@@ -10,15 +13,13 @@ logger = logging.getLogger(__name__)
 def get_connection():
     """
     Establece y retorna una conexión a la base de datos MySQL.
-    
-    NOTA: En un entorno de producción, las credenciales deben ser provistas
-    a través de variables de entorno (ej. usando un archivo .env).
     """
     try:
         connection = mysql.connector.connect(
+            # Si la variable no está en el .env, usará el segundo parámetro como respaldo (opcional)
             host=os.environ.get('DB_HOST', 'localhost'),
             user=os.environ.get('DB_USER', 'root'),
-            password=os.environ.get('DB_PASSWORD', ''),
+            password=os.environ.get('DB_PASSWORD'),
             database=os.environ.get('DB_NAME', 'deposito')
         )
         if connection.is_connected():
